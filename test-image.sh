27511973bc8414ec688a335775a1d8c4b84e96fd
Rouven Hi <rouven@himmelstein.info>
2022-08-14 11:42:50 +0200

    feat: build and push container image

--- test-image.sh
+++ test-image.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 # Description:
-#     This script tests the beammp-server docker image
+#     This script tests the beammp-server container image
 #
 ##################
 
 # Spin up a BeamMP server
 echo "🚀 Spinning up a test container"
-docker run -d --name test-container -e AUTH_KEY="$BEAMMP_AUTH_KEY" "$1"
+podman run -d --name test-container -e AUTH_KEY="$BEAMMP_AUTH_KEY" "$1"
 
 # Wait some time
 echo "😴 sleeping 10 seconds"
@@ -15,17 +15,17 @@ sleep 10
 
 # Test for errors in log
 echo "🧪 Testing for errors"
-docker logs test-container | grep -i '\[ERROR\]'
+podman logs test-container | grep -i '\[ERROR\]'
 if [ "$?" -eq 0 ]; then
   echo "❌ Found errors in server log:"
   echo "======================"
-  docker logs test-container
+  podman logs test-container
   echo "======================"
-  docker stop test-container && docker rm test-container
+  podman stop test-container && podman rm test-container
   exit 1
 fi
 
 # Cleanup and exit with 0
-docker stop test-container && docker rm test-container
+podman stop test-container && podman rm test-container
 echo "✅ Done, everything looks good"
 exit 0
