7ae1541c970648cb56afec3f7ee4e072d27170ae
Ludwig Witzmann <l-witzmann@posteo.de>
2026-04-04 15:56:43 +0200

    Install unzip and added TEMP_SETTINGS_FILE env variable to improve compatibility with BeamJoy(-Sandbox)

--- container-data/entrypoint.sh
+++ container-data/entrypoint.sh
@@ -5,3 +5,8 @@ chmod 777 -R /beammp/ 2> /dev/null
 
 # Start the BeamMP server executable
 /beammp/beammp-server
+
+# Check if TEMP_SETTINGS_FILE is set and overwrite existing config in case such a file exists
+if [ -n "${TEMP_SETTINGS_FILE+isset}" ] && [ -f "/beammp/${TEMP_SETTINGS_FILE}" ]; then
+        cat "/beammp/${TEMP_SETTINGS_FILE}" > /beammp/ServerConfig.toml && rm "/beammp/${TEMP_SETTINGS_FILE}"
+fi
